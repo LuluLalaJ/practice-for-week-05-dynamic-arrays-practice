32f7481bda4a2d0bf83b0f393d328a7e67a9a0f0
Chen <chen.jiang.1206@gmail.com>
2023-06-01 09:32:04 -0400

    done

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -1,21 +1,39 @@
 class DynamicArray {
 
   constructor(defaultSize=4) {
-
     // Your code here
-
+    this.length = 0
+    this.capacity = defaultSize
+    this.data = new Array(defaultSize)
   }
 
   read(index) {
-
     // Your code here
+    if (index >= this.length) {
+      return undefined
+    }
+    return this.data[index]
   }
 
   unshift(val) {
-
     // Your code here
-  }
+    if (this.capacity === this.length) {
+      this.capacity *= 2
+      const newData = new Array(this.capacity)
+      for (let i = 0; i < this.length; i++) {
+        newData[i+1] = this.data[i]
+        this.length++
+      }
+      newData[0] = val
+      this.data = newData
+    }
 
+    for (let i = (this.length - 1); i >= 0; i--) {
+      this.data[i+1] = this.data[i]
+    }
+    this.data[0] = val
+    this.length++
+  }
 }
 
 
